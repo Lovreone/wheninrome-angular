923bb4857b5c08f6001bc6aee405203c93ae2f61
Branislav Lovrenski <branislav.lovrenski@gmail.com>
2022-04-08 13:17:09 +0200

    CMS Landmarks list small code cleanup

--- src/app/content-management/components/landmark-list-modify/landmark-list-modify.component.ts
+++ src/app/content-management/components/landmark-list-modify/landmark-list-modify.component.ts
@@ -46,5 +46,4 @@ export class LandmarkListModifyComponent implements OnInit {
         });
     }
   }
-
 }
--- src/app/shared/services/landmark.service.ts
+++ src/app/shared/services/landmark.service.ts
@@ -54,5 +54,4 @@ export class LandmarkService {
       catchError(() => of(false))
     );
   }
-
 }
