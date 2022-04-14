7ce90ff6336e0f0cfa2f2e6d0bc3e78a4d8694ab
Branislav Lovrenski <branislav.lovrenski@gmail.com>
2022-04-15 00:50:49 +0200

    Portal & CMS - Footer copyright text update

--- src/app/content-management/components/footer-admin/footer-admin.component.ts
+++ src/app/content-management/components/footer-admin/footer-admin.component.ts
@@ -12,4 +12,11 @@ export class FooterAdminComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  getCopyrightYear(): string {
+    const startYear = '2022'
+    const currentYear = new Date().getFullYear().toString();
+    return startYear === currentYear ? 
+      startYear : 
+      startYear.concat('-',currentYear);
+  }
 }
