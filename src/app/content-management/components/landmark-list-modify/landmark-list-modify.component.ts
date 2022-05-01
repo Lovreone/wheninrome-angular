67cdfee1abf643fb97da73650ed8408b3f4ac6f8
Branislav Lovrenski <branislav.lovrenski@gmail.com>
2022-05-01 22:52:37 +0200

    CMS Landmarks: Added Search by name filter

--- src/app/content-management/components/landmark-list-modify/landmark-list-modify.component.ts
+++ src/app/content-management/components/landmark-list-modify/landmark-list-modify.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { LandmarkService } from '../../../shared/services/landmark.service';
@@ -9,10 +9,13 @@ import { Landmark } from '../../../shared/models/landmark.model';
   templateUrl: './landmark-list-modify.component.html',
   styleUrls: ['./landmark-list-modify.component.css']
 })
-export class LandmarkListModifyComponent implements OnInit {
+export class LandmarkListModifyComponent implements OnInit, OnChanges {
 
+  @ViewChild('searchTerm') searchTerm!: ElementRef<HTMLInputElement>;
   @Input() landmarks: Landmark[] = [];
   @Input() isLoading = true;
+
+  fileredLandmarks: Landmark[] = [];
 
   constructor(
     private landmarkService: LandmarkService,
@@ -21,12 +24,28 @@ export class LandmarkListModifyComponent implements OnInit {
 
   ngOnInit(): void { }
 
+  ngOnChanges(): void {
+    this.fileredLandmarks = this.landmarks;
+  }
+
   viewItem(landmark: Landmark): void  {
     this.router.navigateByUrl(`/portal/cities/${landmark.city?.slug}/${landmark.slug}`);
   }
 
   modifyItem(landmark: Landmark): void  {
     this.router.navigateByUrl(`/admin/landmarks/modify/${landmark.id}`);
+  }
+
+  searchByName(searchTerm: string): void {
+    this.fileredLandmarks = searchTerm ? 
+      this.landmarks.filter((item) => 
+        item.name.toLowerCase().includes(searchTerm.toLowerCase())) :
+      this.landmarks;
+  }
+
+  resetSearchFilter(): void {
+    this.searchTerm.nativeElement.value = '';
+    this.fileredLandmarks = this.landmarks;
   }
 
   deleteItem(landmark: Landmark): void {
