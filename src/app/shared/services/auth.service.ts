4db5b266cac1b89d564bb017a9b2bb2a55d11e88
Branislav Lovrenski <branislav.lovrenski@gmail.com>
2022-05-09 14:48:18 +0200

    Auth Service skeleton

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,7 +1,8 @@
+import { Router } from '@angular/router';
 import { Injectable } from "@angular/core";
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 import { baseApiUrl } from 'src/utils/config';
 import { User } from './../models/user.model';
 
@@ -10,27 +11,93 @@ import { User } from './../models/user.model';
 })
 export class AuthService {
 
-    constructor(private http: HttpClient) { }
+  headers = new HttpHeaders().set('Content-Type', 'application/json');
+  currentUser = {};
 
-    login(username: string, password: string): Observable<User> {
-        return this.http.post<any>(`${baseApiUrl}/auth/login`, { username, password }).pipe(
-          map(user => {
-            // login successful if there's a jwt token in the response
-            if (user && user.access_token) {
-              // store user details and jwt token in local storage to keep user logged in between page refreshes
-              localStorage.setItem('currentUser', JSON.stringify(user));
-              //this.currentUserSubject.next(user);
-            }
-            return user;
-          })
-        );
+  constructor(
+    private http: HttpClient,
+    public router: Router
+  ) { }
+
+  // Sign-up
+  signUp(user: User): Observable<any> {
+    let api = `${baseApiUrl}/users`; // `${baseApiUrl}/register-user`;
+    return this.http.post(api, user).pipe(catchError(this.handleError));
+  }
+
+  // Sign-in
+  signIn(user: User) {
+    return this.http
+      .post<any>(`${baseApiUrl}/auth/login`, user) // `${this.endpoint}/signin`
+      .subscribe((res: any) => {
+        localStorage.setItem('access_token', res.token);
+        this.getUserProfile(res._id).subscribe((res) => {
+          this.currentUser = res;
+          this.router.navigate(['user-profile/' + res.msg._id]);
+        });
+      });
+  }
+  getToken() {
+    return localStorage.getItem('access_token');
+  }
+
+  get isLoggedIn(): boolean {
+    let authToken = localStorage.getItem('access_token');
+    return authToken !== null ? true : false;
+  }
+
+  doLogout() {
+    let removeToken = localStorage.removeItem('access_token');
+    if (removeToken == null) {
+      this.router.navigate(['log-in']);
     }
+  }
 
-    // TODO: Implement
-	//$ curl -X POST http://localhost:3000/auth/login -d '{"username": "johnwayne", "password": "changeme"}' -H "Content-Type: application/json"
+  // User profile
+  getUserProfile(id: any): Observable<any> {
+    let api = `${baseApiUrl}/profile`; // `${this.endpoint}/user-profile/${id}`
+    return this.http.get(api, { headers: this.headers }).pipe(
+      map((res) => {
+        return res || {};
+      }),
+      catchError(this.handleError)
+    );
+  }
 
-	//$ curl http://localhost:3000/profile -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2Vybm..."
-    // getProfile(): any {
-    //     return this.http.get(`${baseApiUrl}/profile`, {});
-    // }
+ // Error
+  handleError(error: HttpErrorResponse) {
+    let msg = '';
+    if (error.error instanceof ErrorEvent) {
+      // client-side error
+      msg = error.error.message;
+    } else {
+      // server-side error
+      msg = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    return throwError(msg);
+  }
+
+
+
+// login(username: string, password: string): Observable<User> {
+//   return this.http.post<any>(`${baseApiUrl}/auth/login`, { username, password }).pipe(
+//     map(user => {
+//       // login successful if there's a jwt token in the response
+//       if (user && user.access_token) {
+//         // store user details and jwt token in local storage to keep user logged in between page refreshes
+//         localStorage.setItem('currentUser', JSON.stringify(user));
+//         //this.currentUserSubject.next(user);
+//       }
+//       return user;
+//     })
+//   );
+// }
+
+// TODO: Implement
+// $ curl -X POST http://localhost:3000/auth/login -d '{"username": "johnwayne", "password": "changeme"}' -H "Content-Type: application/json"
+
+//$ curl http://localhost:3000/profile -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2Vybm..."
+// getProfile(): any {
+//     return this.http.get(`${baseApiUrl}/profile`, {});
+// }
 }
