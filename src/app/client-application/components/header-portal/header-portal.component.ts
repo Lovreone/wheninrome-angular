c32e6d19a8839bb0336fcbb157a5ac32eca3379c
Branislav Lovrenski <branislav.lovrenski@gmail.com>
2022-05-18 14:58:15 +0200

    Portal header links isLoggedIn changes

--- src/app/client-application/components/header-portal/header-portal.component.ts
+++ src/app/client-application/components/header-portal/header-portal.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { AuthService } from './../../../shared/services/auth/auth.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header-portal',
   templateUrl: './header-portal.component.html',
   styleUrls: ['./header-portal.component.css']
 })
-export class HeaderPortalComponent implements OnInit {
+export class HeaderPortalComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  isLoggedIn = false;
+  private userSub!: Subscription;
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.userSub = this.authService.user
+      .subscribe((user) => {this.isLoggedIn = !!user});
   }
 
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
+  }
 }
