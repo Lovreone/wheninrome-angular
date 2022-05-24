1d069d68c2b00981f063b7e49cb3a675afa8932e
Branislav Lovrenski <branislav.lovrenski@gmail.com>
2022-05-24 21:19:43 +0200

    CMS: User list management pages done

--- src/app/content-management/components/user-list-modify/user-list-modify.component.ts
+++ src/app/content-management/components/user-list-modify/user-list-modify.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { UserService } from './../../../shared/services/user.service';
+import { User } from './../../../shared/models/user.model';
 
 @Component({
   selector: 'app-user-list-modify',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserListModifyComponent implements OnInit {
 
-  constructor() { }
+  @Input() users: User[] = [];
+  @Input() isLoading = true;
 
-  ngOnInit(): void {
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void { }
+
+  modifyItem(user: User): void  {
+    this.router.navigateByUrl(`/admin/users/modify/${user.id}`);
   }
 
+  deleteItem(user: User): void {
+    // TODO: Temporary (Development only) functionality, disable for Prod
+    if (confirm('Are you sure you want to delete this User?')) {
+      this.userService.deleteUser(user)
+        .subscribe(res => {
+          if (res) {
+            this.users = this.users.filter(item => item.id !== user.id);
+          }
+        });
+    }
+  }
+
+  getFullName(user: User): string {
+    return user.firstName.concat(' ', user.lastName);
+  }
 }
--- src/app/content-management/pages/users-manage/users-manage.component.ts
+++ src/app/content-management/pages/users-manage/users-manage.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserService } from './../../../shared/services/user.service';
+import { User } from './../../../shared/models/user.model';
+import { mockResDelay } from 'src/utils/config';
+
 @Component({
   selector: 'app-users-manage',
   templateUrl: './users-manage.component.html',
@@ -7,9 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersManageComponent implements OnInit {
 
-  constructor() { }
+  users: User[] = [];
+  isLoading = true;
+
+  constructor(
+    private userService: UserService
+  ) { }
 
   ngOnInit(): void {
+    mockResDelay(() => {
+      this.userService.getAllUsers()
+        .subscribe(users => this.users = users);
+      this.isLoading = false;
+    });
   }
-
 }
