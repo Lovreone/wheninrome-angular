8abe1704f1d0edc57d365999c859b7006c866ce8
Branislav Lovrenski <branislav.lovrenski@gmail.com>
2022-05-24 15:17:25 +0200

    CMS: Added routing for the new User section

--- src/app/content-management/content-management-routing.module.ts
+++ src/app/content-management/content-management-routing.module.ts
@@ -6,24 +6,32 @@ import { CitiesManageComponent } from './pages/cities-manage/cities-manage.compo
 import { CityManageComponent } from './pages/city-manage/city-manage.component';
 import { LandmarksManageComponent } from './pages/landmarks-manage/landmarks-manage.component';
 import { LandmarkManageComponent } from './pages/landmark-manage/landmark-manage.component';
+import { UsersManageComponent } from './pages/users-manage/users-manage.component';
+import { UserManageComponent } from './pages/user-manage/user-manage.component';
 
 const routes: Routes = [
-    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-    { path: 'dashboard', component: AdminDashboardComponent },
-    { path: 'landmarks',
-      children: [
-        { path: '', component: LandmarksManageComponent }, 
-        { path: 'create', component: LandmarkManageComponent },
-        { path: 'modify/:landmarkId', component: LandmarkManageComponent },
-      ]
-    }, 
-    { path: 'cities',
-      children: [
-        { path: '', component: CitiesManageComponent }, 
-        { path: 'create', component: CityManageComponent },
-        { path: 'modify/:cityId', component: CityManageComponent }
-      ]
-    }
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: AdminDashboardComponent },
+  { path: 'landmarks',
+    children: [
+      { path: '', component: LandmarksManageComponent }, 
+      { path: 'create', component: LandmarkManageComponent },
+      { path: 'modify/:landmarkId', component: LandmarkManageComponent },
+    ]
+  }, 
+  { path: 'cities',
+    children: [
+      { path: '', component: CitiesManageComponent }, 
+      { path: 'create', component: CityManageComponent },
+      { path: 'modify/:cityId', component: CityManageComponent }
+    ]
+  },
+  { path: 'users',
+    children: [
+      { path: '', component: UsersManageComponent }, 
+      { path: 'modify/:userId', component: UserManageComponent }
+    ]
+  }
 ];
 
 @NgModule({
