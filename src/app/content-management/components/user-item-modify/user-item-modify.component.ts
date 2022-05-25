fe0aea5b80d525074e8b3deec902fc6fd96475c5
Branislav Lovrenski <branislav.lovrenski@gmail.com>
2022-05-25 19:09:34 +0200

    CMS: Update Single User page implementation

--- src/app/content-management/components/user-item-modify/user-item-modify.component.ts
+++ src/app/content-management/components/user-item-modify/user-item-modify.component.ts
@@ -1,15 +1,88 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { EMAIL_REGEX } from './../../../../utils/utils';
+import { UserService } from './../../../shared/services/user.service';
+import { User } from './../../../shared/models/user.model';
 
 @Component({
   selector: 'app-user-item-modify',
   templateUrl: './user-item-modify.component.html',
   styleUrls: ['./user-item-modify.component.css']
 })
-export class UserItemModifyComponent implements OnInit {
+export class UserItemModifyComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  @Input() user!: User;
+  @Input() isLoading!: boolean;
+
+  userForm!: FormGroup;
+  serverErrors!: Array<string>;
+
+  constructor(
+    private router: Router,
+    private userService: UserService
+  ) { }
 
   ngOnInit(): void {
+    this.createForm();
   }
 
+  ngOnChanges(): void {
+    this.fillForm();
+  }
+
+  createForm(): void {
+    this.userForm = new FormGroup({
+      firstName: new FormControl(undefined, Validators.required),
+      lastName: new FormControl(undefined, Validators.required),
+      email: new FormControl(undefined, 
+        [Validators.required, Validators.email, Validators.pattern(EMAIL_REGEX)]
+      ),
+      username: new FormControl(undefined, Validators.required),
+      isActive: new FormControl(true, Validators.required),
+    });
+  }
+
+  fillForm(): void {
+    if (this.user) {
+      this.userForm.get('firstName')?.setValue(this.user.firstName);
+      this.userForm.get('lastName')?.setValue(this.user.lastName);
+      this.userForm.get('email')?.setValue(this.user.email);
+      this.userForm.get('username')?.setValue(this.user.username);
+      this.userForm.get('isActive')?.setValue(this.user.isActive);
+    } 
+  }
+
+  updateUser(): void {
+    const modifiedUser = this.userForm.getRawValue() as User;
+    modifiedUser.id = this.user.id;
+    this.userService.updateUser(modifiedUser)
+      .subscribe(
+        (res) => {
+          this.clearFormAndGoBack();
+        },
+        (err) => {
+          this.serverErrors = err.error.message;
+        });
+  }
+
+  clearFormAndGoBack(): void {
+    this.userForm.reset();
+    this.serverErrors = []; 
+    this.router.navigateByUrl(`admin/users`);
+  }
+
+  isFieldInvalid(fieldName: string): boolean {
+    const formControl = this.userForm.get(fieldName);
+    return formControl ?
+      formControl?.invalid && (formControl?.dirty || formControl?.touched) :
+      false;
+  }
+
+  /** Getters used for cleaner access from Template */
+  get firstName() { return this.userForm.get('firstName'); };
+  get lastName() { return this.userForm.get('lastName'); };
+  get email() { return this.userForm.get('email'); };
+  get username() { return this.userForm.get('username'); };
+  get isActive() { return this.userForm.get('isActive'); };
 }
--- src/app/content-management/pages/user-manage/user-manage.component.ts
+++ src/app/content-management/pages/user-manage/user-manage.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { mockResDelay } from 'src/utils/config';
+import { UserService } from './../../../shared/services/user.service';
+import { User } from './../../../shared/models/user.model';
 
 @Component({
   selector: 'app-user-manage',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserManageComponent implements OnInit {
 
-  constructor() { }
+  user!: User;
+  userId!: string;
+  isLoading = true;
+
+  constructor(
+    private userService: UserService,
+    private route: ActivatedRoute,
+    private router: Router 
+  ) { }
 
   ngOnInit(): void {
+    this.userId = this.route.snapshot.paramMap.get('userId') || '';
+    if (this.userId) {
+      mockResDelay(() => {
+        this.userService.getUserById(this.userId).subscribe(
+          (res) => {
+            this.user = res; 
+          },
+          (err) => {
+            // Use case: Wrong/non-existing item ID in urlPath 
+            this.router.navigate(['not-found'], { relativeTo: this.route.parent }); 
+          });
+          this.isLoading = false
+      });
+    }
   }
-
 }
