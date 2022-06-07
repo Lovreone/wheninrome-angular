c7732e80efacc8c215115f3a553be88785c2a04c
Branislav Lovrenski <branislav.lovrenski@gmail.com>
2022-06-07 15:05:59 +0200

    Tour Service and model created

---
+++ src/app/shared/models/tour.model.ts
@@ -0,0 +1,10 @@
+export interface Tour {
+    id: string;
+    name: string;
+    tourDate: Date; 
+    startingLocation: string;
+    tourNotes: string;
+    userId: string;
+    createdAt: Date;
+    modifiedAt: Date;    
+}
