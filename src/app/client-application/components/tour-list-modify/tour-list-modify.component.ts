84c5531fb13c31c10877553d675598219261889e
Branislav Lovrenski <branislav.lovrenski@gmail.com>
2022-06-07 22:38:53 +0200

    Portal: Tour list manage page completed

--- src/app/client-application/components/tour-list-modify/tour-list-modify.component.ts
+++ src/app/client-application/components/tour-list-modify/tour-list-modify.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TourService } from '../../../shared/services/tour.service';
+import { Tour } from './../../../shared/models/tour.model';
 
 @Component({
   selector: 'app-tour-list-modify',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TourListModifyComponent implements OnInit {
 
-  constructor() { }
+  @Input() tours: Array<Tour> = [];
+  @Input() isLoading = true;
 
-  ngOnInit(): void {
+  constructor(
+    private tourService: TourService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void { }
+
+  viewItem(tour: Tour): void  {
+    this.router.navigateByUrl(`/portal/tours/${tour.id}`); // TODO: Page and Route to be created
   }
 
+  modifyItem(tour: Tour): void  {
+    this.router.navigateByUrl(`/portal/tours/modify/${tour.id}`);
+  }
+
+  deleteItem(tour: Tour): void {
+    // TODO: Temporary (Development only) functionality, disable for Prod
+    if (confirm('Are you sure you want to delete this Tour? This action is irreversible!')) {
+      this.tourService.deleteTour(tour)
+        .subscribe(isDeleted => {
+          if (isDeleted) {
+            this.tours = this.tours.filter(item => item.id !== tour.id);
+          }
+        });
+    }
+  }
 }
--- src/app/client-application/pages/tours-manage-page/tours-manage-page.component.ts
+++ src/app/client-application/pages/tours-manage-page/tours-manage-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { take, exhaustMap } from 'rxjs/operators';
+import { mockResDelay } from 'src/utils/config';
+import { AuthService } from './../../../shared/services/auth/auth.service';
+import { TourService } from '../../../shared/services/tour.service';
+import { Tour } from './../../../shared/models/tour.model';
 
 @Component({
   selector: 'app-tours-page',
@@ -7,9 +12,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToursManagePageComponent implements OnInit {
 
-  constructor() { }
+  tours: Tour[] = [];
+  isLoading = true;
+
+  constructor(
+    private tourService: TourService,
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
+    mockResDelay(() => {
+      this.authService.getUserProfile()
+        .pipe(
+          take(1),
+          exhaustMap((userData) => 
+            this.tourService.getToursByUser(userData.userId)
+          )
+        ).subscribe((userTours) => {
+          this.tours = userTours;
+          this.isLoading = false;
+        });
+    })
   }
-
 }
