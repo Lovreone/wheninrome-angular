e5b1b2f21a77030ea818e8eba1161277bdc9930b
Branislav Lovrenski <branislav.lovrenski@gmail.com>
2022-06-25 17:34:25 +0200

    Small comment cleanup

--- src/app/shared/models/city.model.ts
+++ src/app/shared/models/city.model.ts
@@ -10,11 +10,4 @@ export interface City {
     isActive: boolean;
     createdAt: Date;
     modifiedAt: Date;
-    // https://mongoosejs.com/docs/schematypes.html
-    // https://stackoverflow.com/questions/22244421/how-to-create-mongoose-schema-with-array-of-object-ids
-    // https://stackoverflow.com/questions/59349305/populate-in-nodejs-with-type-schema-types-objectid-return-null
-
-    // https://stackoverflow.com/questions/50980042/convert-string-array-into-object-id-array
-    // https://stackoverflow.com/questions/70315483/mongoose-nested-objectid-array-validation-dto
-    // https://github.com/nestjs/swagger/issues/1112
 }
